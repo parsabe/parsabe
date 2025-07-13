55ebed574d216e4ce609c5eb39aaa77ca68068a4
Parsa Besharat <147280090+parsabe@users.noreply.github.com>
2025-07-13 10:39:37 +0200

    Update install.sh

--- configure/install.sh
+++ configure/install.sh
@@ -100,6 +100,11 @@ wget -O- https://updates.signal.org/desktop/apt/keys.asc | gpg --dearmor > signa
 cat signal-desktop-keyring.gpg | sudo tee /usr/share/keyrings/signal-desktop-keyring.gpg > /dev/null
 
 
+# 2. Add our repository to your list of repositories:
+echo 'deb [arch=amd64 signed-by=/usr/share/keyrings/signal-desktop-keyring.gpg] https://updates.signal.org/desktop/apt xenial main' |\
+  sudo tee /etc/apt/sources.list.d/signal-xenial.list
+
+  
 # 3. Update your package database and install Signal:
 sudo apt update && sudo apt install signal-desktop
 
